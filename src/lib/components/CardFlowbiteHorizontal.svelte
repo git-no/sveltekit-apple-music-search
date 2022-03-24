59eca4849d6214d2c5a008a1fb6dd11cf60ebd0f
Dr. No <mo+ita-git@depeche.email>
2022-03-24 18:19:35 +0100

    ADD Prefetch links

--- src/lib/components/CardFlowbiteHorizontal.svelte
+++ src/lib/components/CardFlowbiteHorizontal.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { goto } from '$app/navigation';
+	// import { goto } from '$app/navigation';
 
 	export let imageURL = '';
 	export let imageAlt = '';
@@ -8,13 +8,15 @@
 	export let viewHref = '';
 	export let trackID = '';
 
-	const onClick = () => {
-		goto(`/song/${trackID}`);
-	};
+	// const onClick = () => {
+	// 	goto(`/song/${trackID}`);
+	// };
 </script>
 
-<button
-	on:click={onClick}
+<a
+	sveltekit:prefetch
+	sveltekit:noscroll
+	href={`/song/${trackID}`}
 	class="flex w-full flex-col items-center rounded-md bg-white shadow-gray-700 transition-all duration-300 hover:bg-gray-100 hover:shadow-xl hover:shadow-gray-700 dark:bg-gray-800/80 dark:hover:bg-gray-800 md:max-w-xl md:flex-row">
 	<img class="h-32 w-full rounded-t-lg object-cover md:h-auto md:w-32 md:rounded-none md:rounded-l-lg" src={imageURL} alt={imageAlt} />
 	<div class="flex flex-col justify-between p-2 pl-4 leading-normal md:text-left">
@@ -22,4 +24,4 @@
 		<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{subTitle}</p>
 		<slot name="audio" />
 	</div>
-</button>
+</a>
